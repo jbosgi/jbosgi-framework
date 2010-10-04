0061c19d7be2cb9430ecf66be65d1e5a7c0ffbe4
Thomas Diesler <thomas.diesler@jboss.com>
2010-10-04 16:13:23 +0200

    Add missing Constants source file

---
+++ core/src/main/java/org/jboss/osgi/framework/Constants.java
@@ -0,0 +1,38 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 021101301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.osgi.framework;
+
+
+/**
+ * The module manager plugin.
+ *
+ * @author thomas.diesler@jboss.com
+ * @since 06Jul2009
+ */
+public interface Constants extends org.osgi.framework.Constants
+{
+   /** The property that defines a comma seperated list of system module identifiers */
+   String PROP_JBOSS_OSGI_SYSTEM_MODULES = "org.jboss.osgi.system.modules";
+
+   /** The module prefix for modules managed by the OSGi layer */
+   String MODULE_PREFIX = "jbosgi";
+}
