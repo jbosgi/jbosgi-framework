cdd90867fc43907ca3ec7a5b1940230f97ee54a6
Thomas Diesler <thomas.diesler@jboss.com>
2010-12-21 16:12:20 +0100

    [TCK] ActivationPolicy excluded packages processing

--- core/src/main/java/org/jboss/osgi/framework/loading/LazyActivationLocalLoader.java
+++ core/src/main/java/org/jboss/osgi/framework/loading/LazyActivationLocalLoader.java
@@ -24,6 +24,7 @@ package org.jboss.osgi.framework.loading;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.jboss.logging.Logger;
 import org.jboss.modules.DependencySpec;
@@ -32,12 +33,12 @@ import org.jboss.modules.Module;
 import org.jboss.modules.ModuleIdentifier;
 import org.jboss.modules.ModuleLoadException;
 import org.jboss.modules.PathFilter;
-import org.jboss.modules.PathFilters;
 import org.jboss.modules.Resource;
 import org.jboss.osgi.framework.bundle.BundleManager;
 import org.jboss.osgi.framework.bundle.HostBundle;
 import org.jboss.osgi.framework.bundle.OSGiModuleLoader;
 import org.jboss.osgi.framework.plugin.ModuleManagerPlugin;
+import org.osgi.framework.BundleException;
 
 /**
  * A local loader that activates the associated host bundle.
@@ -56,9 +57,11 @@ public class LazyActivationLocalLoader implements LocalLoader
 
    // The dependencies excluding the local dependency
    private final List<DependencySpec> moduleDependencies;
-   private PathFilter lazyPackagesFilter;
-   
-   public LazyActivationLocalLoader(HostBundle hostBundle, ModuleIdentifier identifier, List<DependencySpec> moduleDependencies)
+   private final PathFilter lazyPackagesFilter;
+   private final AtomicBoolean relinkModule = new AtomicBoolean(true);
+   private final AtomicBoolean activateHost = new AtomicBoolean(true);
+
+   public LazyActivationLocalLoader(HostBundle hostBundle, ModuleIdentifier identifier, List<DependencySpec> moduleDependencies, PathFilter lazyFilter)
    {
       this.moduleDependencies = moduleDependencies;
       this.hostBundle = hostBundle;
@@ -66,29 +69,30 @@ public class LazyActivationLocalLoader implements LocalLoader
 
       BundleManager bundleManager = hostBundle.getBundleManager();
       moduleManager = bundleManager.getPlugin(ModuleManagerPlugin.class);
-      lazyPackagesFilter = hostBundle.getLazyPackagesFilter();
+      lazyPackagesFilter = lazyFilter;
    }
 
    @Override
    public Class<?> loadClassLocal(String className, boolean resolve)
    {
+      Class<?> definedClass = null;
+      
       String pathForClassName = getPathForClassName(className);
-      if (lazyPackagesFilter.accept(pathForClassName))
+      if (lazyPackagesFilter.accept(pathForClassName) )
       {
-         log.debugf("Trigger lazy activation on class load: %s", className);
+         // Relink the module and load the requested class
          try
          {
-            lazyPackagesFilter = PathFilters.rejectAll();
             Module module = moduleManager.getModule(identifier);
-            OSGiModuleLoader moduleLoader = moduleManager.getModuleLoader();
-            List<DependencySpec> dependencies = new ArrayList<DependencySpec>(moduleDependencies);
-            dependencies.add(DependencySpec.createLocalDependencySpec());
-            moduleLoader.setAndRelinkDependencies(module, dependencies);
-            Class<?> definedClass = module.getClassLoader().loadClass(className);
-            
-            hostBundle.activateOnClassLoad(definedClass);
-            
-            return definedClass;
+            if (relinkModule.getAndSet(false))
+            {
+               log.debugf("Relinking module [%s] on class load: %s", module, className);
+               OSGiModuleLoader moduleLoader = moduleManager.getModuleLoader();
+               List<DependencySpec> dependencies = new ArrayList<DependencySpec>(moduleDependencies);
+               dependencies.add(DependencySpec.createLocalDependencySpec());
+               moduleLoader.setAndRelinkDependencies(module, dependencies);
+            }
+            definedClass = module.getClassLoader().loadClass(className);
          }
          catch (ModuleLoadException ex)
          {
@@ -96,11 +100,24 @@ public class LazyActivationLocalLoader implements LocalLoader
          }
          catch (ClassNotFoundException ex)
          {
-            // ignore
+            return null;
+         }
+         
+         // Activate the bundle lazily
+         if (activateHost.getAndSet(false) && definedClass != null)
+         {
+            try
+            {
+               log.debugf("Trigger lazy activation on class load: %s", className);
+               hostBundle.activateOnClassLoad(definedClass);
+            }
+            catch (BundleException ex)
+            {
+               log.errorf(ex, "Cannot activate lazily: %s", hostBundle);
+            }
          }
       }
-
-      return null;
+      return definedClass;
    }
 
    private String getPathForClassName(String className)
