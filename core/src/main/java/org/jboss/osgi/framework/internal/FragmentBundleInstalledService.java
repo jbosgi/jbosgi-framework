822c7829eeb7d475af58aa87ba7f95e5e5caa7cd
Rico Neubauer <r.neubauer@seeburger.de>
2012-05-18 17:03:17 +0200

    JBOSGI-555 Cannot activate OSGi subsystem with fragment bundle deployments

--- core/src/main/java/org/jboss/osgi/framework/internal/FragmentBundleInstalledService.java
+++ core/src/main/java/org/jboss/osgi/framework/internal/FragmentBundleInstalledService.java
@@ -5,16 +5,16 @@
  * Copyright (C) 2010 - 2012 JBoss by Red Hat
  * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 2.1 of the 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
+ *
+ * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-2.1.html>.
  * #L%
@@ -43,9 +43,11 @@
 package org.jboss.osgi.framework.internal;
 
 import org.jboss.msc.service.ServiceBuilder;
+import org.jboss.msc.service.ServiceListener;
 import org.jboss.msc.service.ServiceName;
 import org.jboss.msc.service.ServiceTarget;
 import org.jboss.osgi.deployment.deployer.Deployment;
+import org.osgi.framework.Bundle;
 import org.jboss.osgi.framework.StorageState;
 import org.jboss.osgi.metadata.OSGiMetaData;
 import org.osgi.framework.BundleException;
@@ -58,11 +60,14 @@ import org.osgi.framework.BundleException;
  */
 final class FragmentBundleInstalledService extends UserBundleInstalledService<FragmentBundleState,FragmentBundleRevision> {
 
-    static ServiceName addService(ServiceTarget serviceTarget, FrameworkState frameworkState, Deployment dep) throws BundleException {
+    static ServiceName addService(ServiceTarget serviceTarget, FrameworkState frameworkState, Deployment dep, ServiceListener<Bundle> listener) throws BundleException {
         ServiceName serviceName = frameworkState.getBundleManager().getServiceName(dep).append("INSTALLED");
         FragmentBundleInstalledService service = new FragmentBundleInstalledService(frameworkState, dep);
         ServiceBuilder<FragmentBundleState> builder = serviceTarget.addService(serviceName, service);
         builder.addDependency(InternalServices.FRAMEWORK_CORE_SERVICES);
+        if (listener != null) {
+            builder.addListener(listener);
+        }
         builder.install();
         return serviceName;
     }
