97963060617a06d8364f7545c4ae6d9f422c17fc
Thomas Diesler <thomas.diesler@jboss.com>
2012-07-13 10:53:51 +0200

    Remove old license headers

--- itest/src/test/java/org/jboss/test/osgi/framework/classloader/DynamicImportLoadOrderTestCase.java
+++ itest/src/test/java/org/jboss/test/osgi/framework/classloader/DynamicImportLoadOrderTestCase.java
@@ -5,44 +5,21 @@
  * Copyright (C) 2010 - 2012 JBoss by Red Hat
  * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 2.1 of the 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
+ *
+ * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-2.1.html>.
  * #L%
  */
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2009, Red Hat Middleware LLC, and individual contributors
- * as indicated by the @author tags. See the copyright.txt file in the
- * distribution for a full listing of individual contributors.
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
-package org.jboss.test.osgi.framework.jbosgi323;
-
-// 
+package org.jboss.test.osgi.framework.classloader;
 
 import org.jboss.osgi.testing.OSGiFrameworkTest;
 import org.jboss.osgi.spi.OSGiManifestBuilder;
@@ -58,34 +35,29 @@ import java.io.InputStream;
 
 /**
  * [JBOSGI-323] DynamicImport-Package takes presendence over embedded classes
- * 
+ *
  * https://jira.jboss.org/jira/browse/JBOSGI-323
- * 
+ *
  * @author thomas.diesler@jboss.com
  * @since 04-May-2010
  */
-public class OSGi323TestCase extends OSGiFrameworkTest {
+public class DynamicImportLoadOrderTestCase extends OSGiFrameworkTest {
 
     @Test
     public void testStaticImport() throws Exception {
-        // Bundle-SymbolicName: jbosgi323-bundleA
-        // Import-Package: org.jboss.test.osgi.framework.classloader.support.a
+
         final JavaArchive archiveA = ShrinkWrap.create(JavaArchive.class, "jbosgi323-bundleA");
         archiveA.addClass(A.class);
         archiveA.setManifest(new Asset() {
-
             public InputStream openStream() {
                 OSGiManifestBuilder builder = OSGiManifestBuilder.newInstance();
                 builder.addBundleManifestVersion(2);
                 builder.addBundleSymbolicName(archiveA.getName());
-                builder.addImportPackages("org.jboss.test.osgi.framework.classloader.support.a");
+                builder.addImportPackages(A.class);
                 return builder.openStream();
             }
         });
 
-        // Bundle-SymbolicName: jbosgi323-bundleB
-        // Export-Package: org.jboss.test.osgi.framework.classloader.support.a,
-        // org.jboss.test.osgi.container.classloader.support.b
         final JavaArchive archiveB = ShrinkWrap.create(JavaArchive.class, "jbosgi323-bundleB");
         archiveB.addClasses(A.class, B.class);
         archiveB.setManifest(new Asset() {
@@ -94,8 +66,7 @@ public class OSGi323TestCase extends OSGiFrameworkTest {
                 OSGiManifestBuilder builder = OSGiManifestBuilder.newInstance();
                 builder.addBundleManifestVersion(2);
                 builder.addBundleSymbolicName(archiveB.getName());
-                builder.addExportPackages("org.jboss.test.osgi.framework.classloader.support.a");
-                builder.addExportPackages("org.jboss.test.osgi.framework.classloader.support.b");
+                builder.addExportPackages(A.class, B.class);
                 return builder.openStream();
             }
         });
@@ -119,34 +90,26 @@ public class OSGi323TestCase extends OSGiFrameworkTest {
 
     @Test
     public void testDynamicImportWithPackage() throws Exception {
-        // Bundle-SymbolicName: jbosgi323-bundleA
-        // DynamicImport-Package: org.jboss.test.osgi.framework.classloader.support.a
         final JavaArchive archiveA = ShrinkWrap.create(JavaArchive.class, "jbosgi323-bundleA");
         archiveA.addClass(A.class);
         archiveA.setManifest(new Asset() {
-
             public InputStream openStream() {
                 OSGiManifestBuilder builder = OSGiManifestBuilder.newInstance();
                 builder.addBundleManifestVersion(2);
                 builder.addBundleSymbolicName(archiveA.getName());
-                builder.addDynamicImportPackages("org.jboss.test.osgi.framework.classloader.support.a");
+                builder.addDynamicImportPackages(A.class.getPackage().getName());
                 return builder.openStream();
             }
         });
 
-        // Bundle-SymbolicName: jbosgi323-bundleB
-        // Export-Package: org.jboss.test.osgi.framework.classloader.support.a,
-        // org.jboss.test.osgi.container.classloader.support.b
         final JavaArchive archiveB = ShrinkWrap.create(JavaArchive.class, "jbosgi323-bundleB");
         archiveB.addClasses(A.class, B.class);
         archiveB.setManifest(new Asset() {
-
             public InputStream openStream() {
                 OSGiManifestBuilder builder = OSGiManifestBuilder.newInstance();
                 builder.addBundleManifestVersion(2);
                 builder.addBundleSymbolicName(archiveB.getName());
-                builder.addExportPackages("org.jboss.test.osgi.framework.classloader.support.a");
-                builder.addExportPackages("org.jboss.test.osgi.framework.classloader.support.b");
+                builder.addExportPackages(A.class, B.class);
                 return builder.openStream();
             }
         });
@@ -170,8 +133,6 @@ public class OSGi323TestCase extends OSGiFrameworkTest {
 
     @Test
     public void testDynamicImportWithWildcard() throws Exception {
-        // Bundle-SymbolicName: jbosgi323-bundleA
-        // DynamicImport-Package: *
         final JavaArchive archiveA = ShrinkWrap.create(JavaArchive.class, "jbosgi323-bundleA");
         archiveA.addClass(A.class);
         archiveA.setManifest(new Asset() {
@@ -184,9 +145,6 @@ public class OSGi323TestCase extends OSGiFrameworkTest {
             }
         });
 
-        // Bundle-SymbolicName: jbosgi323-bundleB
-        // Export-Package: org.jboss.test.osgi.framework.classloader.support.a,
-        // org.jboss.test.osgi.container.classloader.support.b
         final JavaArchive archiveB = ShrinkWrap.create(JavaArchive.class, "jbosgi323-bundleB");
         archiveB.addClasses(A.class, B.class);
         archiveB.setManifest(new Asset() {
@@ -194,8 +152,7 @@ public class OSGi323TestCase extends OSGiFrameworkTest {
                 OSGiManifestBuilder builder = OSGiManifestBuilder.newInstance();
                 builder.addBundleManifestVersion(2);
                 builder.addBundleSymbolicName(archiveB.getName());
-                builder.addExportPackages("org.jboss.test.osgi.framework.classloader.support.a");
-                builder.addExportPackages("org.jboss.test.osgi.framework.classloader.support.b");
+                builder.addExportPackages(A.class, B.class);
                 return builder.openStream();
             }
         });
