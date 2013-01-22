194b29b88706e76f4a4bc706f2df41baa7a8b890
Thomas Diesler <thomas.diesler@jboss.com>
2013-01-22 15:53:27 +0100

    [AS7-6211] OSGi bundles show version 0.0.0 in AS7

--- core/src/main/java/org/jboss/osgi/framework/spi/AbstractBundleAdaptor.java
+++ core/src/main/java/org/jboss/osgi/framework/spi/AbstractBundleAdaptor.java
@@ -49,6 +49,7 @@ import org.jboss.osgi.metadata.OSGiMetaData;
 import org.jboss.osgi.resolver.XBundle;
 import org.jboss.osgi.resolver.XBundleRevision;
 import org.jboss.osgi.resolver.XEnvironment;
+import org.jboss.osgi.resolver.XIdentityCapability;
 import org.jboss.osgi.resolver.spi.AbstractElement;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
@@ -56,10 +57,12 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.Version;
+import org.osgi.framework.namespace.IdentityNamespace;
 import org.osgi.framework.startlevel.BundleStartLevel;
 import org.osgi.framework.wiring.BundleRevision;
 import org.osgi.framework.wiring.BundleRevisions;
 import org.osgi.framework.wiring.BundleWiring;
+import org.osgi.resource.Capability;
 
 /**
  * An abstract implementation that adapts a {@link Module} to a {@link Bundle}
@@ -112,7 +115,15 @@ public class AbstractBundleAdaptor extends AbstractElement implements XBundle, L
 
     @Override
     public String getSymbolicName() {
-        return module.getIdentifier().getName();
+        String symbolicName = null;
+        List<Capability> icaps = brev.getCapabilities(IdentityNamespace.IDENTITY_NAMESPACE);
+        if (icaps.size() > 0) {
+            XIdentityCapability icap = (XIdentityCapability) icaps.get(0);
+            symbolicName = icap.getSymbolicName();
+        } else {
+            symbolicName = module.getIdentifier().getName();
+        }
+        return symbolicName;
     }
 
     @Override
@@ -127,12 +138,20 @@ public class AbstractBundleAdaptor extends AbstractElement implements XBundle, L
 
     @Override
     public Version getVersion() {
-        String slot = module.getIdentifier().getSlot();
-        try {
-            return Version.parseVersion(slot);
-        } catch (IllegalArgumentException ex) {
-            return Version.emptyVersion;
+        Version version = Version.emptyVersion;
+        List<Capability> icaps = brev.getCapabilities(IdentityNamespace.IDENTITY_NAMESPACE);
+        if (icaps.size() > 0) {
+            XIdentityCapability icap = (XIdentityCapability) icaps.get(0);
+            version = icap.getVersion();
+        } else {
+            String slot = module.getIdentifier().getSlot();
+            try {
+                version = Version.parseVersion(slot);
+            } catch (IllegalArgumentException ex) {
+                // ignore
+            }
         }
+        return version;
     }
 
     @Override
